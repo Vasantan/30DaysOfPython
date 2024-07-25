fc178f5825eba46cf87028b1fe5c0502cbf53e25
Vasantan <vasa5431@gmail.com>
2024-07-25 14:40:59 +0800

    2nd day exercise

---
+++ day_2/variables.py
@@ -0,0 +1,66 @@
+# Day 2: 30 Days of python programming
+
+#Exercise: 1
+first_name = "Vasantan"
+last_name = "Govinda Raju"
+full_name = "Vasantan Govinda Raju"
+country = "Malaysia"
+city = "Johor Bahru"
+age = 41
+year =2024
+is_married = True
+is_true = True
+is_light = False
+x,y,z = "string",10,True
+
+#Exercise : Level 2
+#1
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light))
+print(type(x))
+print(type(y))
+print(type(z))
+#2
+print(len(first_name))
+#3
+print(len(last_name)-len(first_name))
+#4
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product =  num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+#5
+radius = 30
+area_of_circle = 3.14*radius**2
+circum_of_circle = 2*3.14*radius
+print(area_of_circle)
+print(circum_of_circle)
+radius =  int(input("Input your circle radius:"))
+area_of_circle = 3.14*radius**2
+print(f"Area of circle : {area_of_circle}")
+
+first_name = input("please provide your first name:")
+last_name = input("please provide your last name:")
+country = input("please provide your country name:")
+age = input("Please provide your age:")
+print (first_name)
+print(last_name)
+print(country)
+print(age)
+
+
+
